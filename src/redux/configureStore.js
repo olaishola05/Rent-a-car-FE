c067c0d324cc3d5f89bdd2dcd06c7b0050b23c5b
Atsighi Bright <brytebee@gmail.com>
2022-06-10 14:59:56 +0100

    :white_check_mark: Add details page
    git push origin  -u display_item

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,19 @@
 import {
-  legacy_createStore as createStore, combineReducers, applyMiddleware, compose,
+  legacy_createStore as createStore,
+  combineReducers,
+  applyMiddleware,
+  compose,
 } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import userReducer from './reducers/userReducer';
 import carsReducer from './reducers/cars/carsReducer';
+import carReducer from './reducers/cars/carReducer';
 
 const rootReducer = combineReducers({
   user: userReducer,
   cars: carsReducer,
+  car: carReducer,
 });
 
 const store = createStore(rootReducer, compose(applyMiddleware(logger, thunk)));
---
+++ src/redux/reducers/cars/carReducer.js
@@ -0,0 +1,24 @@
+const DISPLAY_CAR = 'RENT_A_CAR_FE/CARS/DISPLAY_CAR';
+const URL = 'http://localhost:3001/api/v1';
+
+const displayCar = (payload) => ({
+  type: DISPLAY_CAR,
+  payload,
+});
+
+export const fetchCarFromDB = (id) => async (dispatch) => {
+  const req = await (await fetch(`${URL}/cars/${id}`)).json();
+  console.log('reducer', req);
+  dispatch(displayCar(req));
+};
+
+const carReducer = (state = [], action) => {
+  switch (action.type) {
+    case DISPLAY_CAR:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default carReducer;
