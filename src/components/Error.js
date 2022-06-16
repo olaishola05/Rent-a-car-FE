a5331ec17c0a92d93fce73ed4996b9da6cb6d810
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-06-16 17:50:19 +0530

    Style Error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,20 +3,29 @@ import { NavLink } from 'react-router-dom';
 
 function Error() {
   return (
-    <div className="d-flex align-items-center justify-content-center vh-100">
-      <div className="text-center">
-        <h1 className="display-1 fw-bold">404</h1>
-        <p className="fs-3">
-          {' '}
-          <span className="text-danger">Opps!</span>
-          {' '}
-          Page not found.
-        </p>
-        <p className="lead">
-          You are not authorized to view this page.
-        </p>
-
-        <NavLink to="/" className="btn btn-primary">Go To Home</NavLink>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col">
+          <div className="d-flex align-items-center justify-content-center pt-5">
+            <div className="text-center">
+              <h1 className="display-1 fw-bold">404</h1>
+              <p className="fs-3">
+                {' '}
+                <span className="text-danger">Opps!</span>
+                {' '}
+                Page not found.
+              </p>
+              <br />
+              <p className="lead">
+                You are not authorized to view this page.
+                <br />
+                Login to view this page.
+              </p>
+              <br />
+              <NavLink to="/" className="btn btn-secondary">Go Back</NavLink>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
